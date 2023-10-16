ef9889f8f99e1e3c59f99a681820b35bbcc5eacd
nazaretl <nazaretl@hu-berlin.de>
2023-10-16 20:49:57 +0200

    new

--- noxfile.py
+++ noxfile.py
@@ -2,14 +2,16 @@ import nox
 
 nox.options.sessions = ["lint", "tests"]
 
+
 @nox.session(python=["3.8", "3.12"])
 def tests(session: nox.Session) -> None:
     """
     Run the unit and regular tests.
     """
     session.install("-e.[test]")
-    session.run("pytest", *session.posargs
-            )
+    session.run("pytest", *session.posargs)
+
+
 @nox.session
 def lint(session: nox.Session) -> None:
     """
